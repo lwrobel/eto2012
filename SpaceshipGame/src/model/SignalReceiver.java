8cd6fce2d01269b14dbe79a75f67608205bfeae6
Aleksandra Spyra <aleksandra.spyra180@gamail.com>
2012-11-15 00:46:06 +0100

    SignalReceiver class added

---
+++ SpaceshipGame/src/model/SignalReceiver.java
@@ -0,0 +1,13 @@
+package model;
+
+/**
+ * The Class SignalReceiver.
+ * 
+ * This class will be used to receive signals such as screen touch.
+ * It will exchange information with Controller.
+ *
+ * @author Aleksandra Spyra
+ */
+
+public class SignalReceiver {
+}

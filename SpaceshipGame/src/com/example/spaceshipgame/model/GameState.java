4be925308fed9d79f092268aeea87d0d19becf72
Aleksandra Spyra <aleksandra.spyra180@gamail.com>
2012-11-18 17:57:08 +0100

    GameState and Player(empty)

---
+++ SpaceshipGame/src/com/example/spaceshipgame/model/GameState.java
@@ -0,0 +1,9 @@
+package com.example.spaceshipgame.model;
+
+/**
+ * The Class GameState.
+ * @author Aleksandra Spyra
+ */
+public class GameState {
+	Player[] players;
+}
---
+++ SpaceshipGame/src/com/example/spaceshipgame/model/Player.java
@@ -0,0 +1,12 @@
+package com.example.spaceshipgame.model;
+
+
+/**
+ * The Class Player.
+ *
+ * Someone else task is to fill it in. 
+ * I have just needed it for GameState.
+ */
+public class Player {
+
+}

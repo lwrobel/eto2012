e8109d53a7bb948e1cf62b19a1b13a99eae54424
shajen <shajen@shajen.pl>
2012-12-10 23:27:05 +0100

    bugfix: game crashes, when resume after pause

--- SpaceshipGame/src/com/example/spaceshipgame/GameView.java
+++ SpaceshipGame/src/com/example/spaceshipgame/GameView.java
@@ -4,27 +4,30 @@ import com.example.spaceshipgame.controller.Controller;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	private GameThread	thread;
+	private Controller	controller;
 
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		SurfaceHolder holder = getHolder();
 		holder.addCallback(this);
-		thread = new GameThread(holder);
 	}
 
 	public void setController(Controller controller) {
-		thread.setController(controller);
+		this.controller = controller;
 	}
 
-	public void surfaceChanged(SurfaceHolder holder, int format, int width,	int height) {
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
 	}
 
 	public void surfaceCreated(SurfaceHolder holder) {
+		thread = new GameThread(holder);
+		thread.setController(controller);
 		thread.setRunning(true);
 		thread.start();
 	}

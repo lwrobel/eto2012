2bc9b71562ae9bf0d830a6b52c26386848b3b2f6
Aleksandra Spyra <aleksandra.spyra180@gamail.com>
2012-12-14 19:34:03 +0100

    dobrze jest nie generować magicznych zmiennych

--- SpaceshipGame/src/com/example/spaceshipgame/model/Map.java
+++ SpaceshipGame/src/com/example/spaceshipgame/model/Map.java
@@ -3,10 +3,15 @@ package com.example.spaceshipgame.model;
 import android.graphics.Point;
 
 public class Map {
-	public final Point	MARGIN	= new Point(100, 50);
+	private static final int 	MARGIN_SIZE_X	= 100;
+	private static final int 	MARGIN_SIZE_Y	= 50;
+	private static final int 	MAP_SIZE_X		= 3000;
+	private static final int 	MAP_SIZE_Y		= 1500;
+	
+	public final Point	MARGIN	= new Point(MARGIN_SIZE_X, MARGIN_SIZE_Y);
 	public Point		size;
 
 	public Map() {
-		size = new Point(3000, 1500);
+		size = new Point(MAP_SIZE_X, MAP_SIZE_Y);
 	}
 }
